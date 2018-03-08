039e6e199c81dc0aef65a7f900525adbf05bd1c3
arifnpm <arif.masyur@candelalabs.io>
2018-03-09 00:58:39 +0800

    jet query and user profile

--- prubot/app.js
+++ prubot/app.js
@@ -5,6 +5,7 @@ const busboy = require('connect-busboy');
 const fs = require('fs');
 const PruBot = require('./prubot').PruBot;
 const { OCRService } = require('./ocr');
+const fetch = require('node-fetch');
 
 class Application {
     constructor(port, bots) {
@@ -15,6 +16,18 @@ class Application {
         this.bot = bots.zh;
     }
 
+    async queryProfile(hkID) {
+        const result = await fetch(`http://server7.imorum.com:3001/user/${hkID}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const answer = await result.json();
+        console.log(answer);
+        return answer;
+    }
+
     start() {
         this.app.use(busboy({
             highWaterMark: 10 * 1024 * 1024,
@@ -22,8 +35,8 @@ class Application {
                 fileSize: 20 * 1024 * 1024
             }
         }));
-        this.app.use(bodyParser.json({limit: '10mb', type: 'application/json'}));
-        this.app.use(bodyParser.urlencoded({limit: '10mb'}));
+        this.app.use(bodyParser.json({ limit: '10mb', type: 'application/json' }));
+        this.app.use(bodyParser.urlencoded({ limit: '10mb' }));
         this.app.use((err, req, res, next) => {
             console.error(err.stack);
             console.log(res);
@@ -44,7 +57,104 @@ class Application {
         });
 
         this.app.get('/claim/:userId', (req, res) => {
-            res.status(200).send({amount: '5000 HKD'});
+            res.status(200).send({ amount: '5000 HKD' });
+        });
+
+        this.app.get('/userprofile/:hkID', async (req, res) => {
+            res.status(200).send(await this.queryProfile(req.params.hkID));
+        });
+
+        /**
+         * Try using body
+         * {
+	     *    "hkID": "C823494",
+	     *    "amount": 1500
+         * }
+         * 
+         */
+        this.app.post('/queryjet/', async (req, res) => {
+            const profile = await this.queryProfile(req.body.hkID);
+            let jsonRequest = {
+                "request": {
+                    "chatbotQuestion": "Am I eligible for the procedure",
+                    "currentClaim": {
+                        "claimID": "1000001",
+                        "policyID": profile.result.data.profile.policies[0].policyID,
+                        "type": "Medical",
+                        "deathclaimIndicator": null,
+                        "disabilityIndicator": null,
+                        "criticalIllnessIndicator": null,
+                        "medicalClaimsIndicator": null,
+                        "accidentIndicator": null,
+                        "medicalWastageScore": 10517320,
+                        "hospitalizationNecessaryScore": 0.0,
+                        "preExistingConditiionScore": 10517320,
+                        "policyNonDIsclosureScore": 10517320,
+                        "billedAmountValue": req.body.amount,
+                        "billAmountCurrency": "HKD",
+                        "submissionDate": "2016-08-16T12:42:28.000+0000",
+                        "approvedDate": "2016-08-16T12:42:28.000+0000",
+                        "claimRedFlagCategoryList": [
+                            ""
+                        ],
+                        "benefitCodes": [
+                            null
+                        ],
+                        "claimEvents": [
+                            {
+                                "eventStartDate": "2016-08-16T12:42:28.000+0000",
+                                "eventEndDate": "2016-08-16T12:42:28.000+0000",
+                                "eventType": "01",
+                                "eventId": "123XYZ",
+                                "diagnosisList": [
+                                    {
+                                        "code": "455.6", // can get this value from ocr to the mapped category value?
+                                        "type": null,
+                                        "description": null,
+                                        "firstDetectionDate": "2016-08-16T12:42:28.000+0000",
+                                        "maxClaimPeriodinDays": 0,
+                                        "diagnosisJETEligibilityStatus": null
+                                    }
+                                ],
+                                "medicalCenter": {
+                                    "code": "115",
+                                    "name": null,
+                                    "geographicArea": null,
+                                    "medicalCenterType": null
+                                },
+                                "treatments": []
+                            }
+                        ],
+                        "unstructuredDataFlag": null,
+                        "policyAndBenefitElibigilityStatus": null,
+                        "benefitTypeList": [
+                            {
+                                "benefitCode": 3,
+                                "benefitType": null,
+                                "benefitDate": "2016-08-16T12:42:28.000+0000"
+                            }
+                        ],
+                        "claimHospitalInDate": "2016-08-16T12:42:28.000+0000",
+                        "claimHospitalOutDate": "2016-08-16T12:42:28.000+0000"
+                    },
+                    "policy": [
+                        profile.result.data.profile.policies[0]
+                    ],
+                    "previousClaims": [],
+                    "evalStatus": null,
+                    "decision": null
+                },
+                "__DecisionID__": "123456789abcXYZ"
+            }
+            const result = await fetch(`http://54.179.151.37/DecisionService/rest/v1/PHKLClaimProcessingRuleApp/ClaimProcessingRuleService`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(jsonRequest)
+            });
+            const answer = await result.json();
+            res.status(200).send(answer);
         });
 
         this.app.get('/chathistory/:userId', async (req, res) => {
@@ -60,7 +170,7 @@ class Application {
                     let fstream = fs.createWriteStream('/tmp/' + savedFileName);
                     file.pipe(fstream);
                     fstream.on('close', function () {
-                        res.json({fileId: savedFileName});
+                        res.json({ fileId: savedFileName });
                     });
                 });
                 req.pipe(req.busboy);
@@ -83,7 +193,7 @@ if (require.main === module) {
     let ocrService = new OCRService('AIzaSyBCmBskZqIYZAMi3CjB4cNRqJOv3K-sTHQ');
     let bot = new PruBot(process.env.APIAI_ACCESS_TOKEN || 'a032527b1630406cabc35ded607bfba3', 'en', ocrService);
     let botZh = new PruBot(process.env.APIAI_ACCESS_TOKEN || 'a8d3cedea1c14ca0984c63622c391494', 'zh-HK', ocrService);
-    new Application(3000, {en: bot, zh: botZh}).start();
+    new Application(3000, { en: bot, zh: botZh }).start();
     process.on('uncaughtException', (exception) => {
         console.error('uncaughtException: ', exception);
         process.exit(1);
